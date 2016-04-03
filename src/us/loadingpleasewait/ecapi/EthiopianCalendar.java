f810e95999e9b89181ec80e327a83262c405b935
LoadingPleaseWait <michael@murphey.org>
2016-04-03 12:06:21 -0500

    Use Java 7

---
+++ src/us/loadingpleasewait/ecapi/EthiopianCalendar.java
@@ -0,0 +1,84 @@
+package us.loadingpleasewait.ecapi;
+
+import java.util.Calendar;
+
+/**
+ * Ethiopian calendar system
+ */
+public class EthiopianCalendar extends Calendar {
+
+	private static final long serialVersionUID = 1017426264693286826L;
+
+	/* (non-Javadoc)
+	 * @see java.util.Calendar#computeTime()
+	 */
+	@Override
+	protected void computeTime() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Calendar#computeFields()
+	 */
+	@Override
+	protected void computeFields() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Calendar#add(int, int)
+	 */
+	@Override
+	public void add(int field, int amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Calendar#roll(int, boolean)
+	 */
+	@Override
+	public void roll(int field, boolean up) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Calendar#getMinimum(int)
+	 */
+	@Override
+	public int getMinimum(int field) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Calendar#getMaximum(int)
+	 */
+	@Override
+	public int getMaximum(int field) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Calendar#getGreatestMinimum(int)
+	 */
+	@Override
+	public int getGreatestMinimum(int field) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Calendar#getLeastMaximum(int)
+	 */
+	@Override
+	public int getLeastMaximum(int field) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
